13d1158aab292910a32b5a0b6b93a61a9ce93a9a
SpectronicAS <aidan@spectronic.co.uk>
2026-01-06 14:58:58 +0000

    created requirements, added test data and added functionality to read and process data

---
+++ logic/fileprocessor.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy.signal import find_peaks
+
+def parseFile(filepath):
+    with open(filepath) as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def wavelengthResults(lines):
+    wavelengths = []
+    absorbances = []
+    wlpeaks = []
+    for line in lines[6:-4]:
+        wl, ab = line.split()
+        wavelengths.append(float(wl))
+        absorbances.append(float(ab))
+    peaks, _ = find_peaks(absorbances, height=0.3)
+    for peak in peaks:
+        wlpeaks.append(round(wavelengths[peak], 2))
+    return wlpeaks
+
+
+def absorbanceResults(lines):
+    absorbances = []
+    for line in lines[49:-17]:
+        _, ab = line.split("=")
+        absorbances.append(float(ab))
+    return absorbances
