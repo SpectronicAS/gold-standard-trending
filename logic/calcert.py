b28d8ef66b8a4509f0ce4f4f1ea6f08ae06da5ca
aidybobs <aidanstark@hotmail.com>
2026-01-06 19:11:50 +0000

    added wavelength results to calcert need to fix absresults

--- logic/calcert.py
+++ logic/calcert.py
@@ -1,3 +1,24 @@
 from fileprocessor import wavelengthResults, absorbanceResults
-from engineerset import EngineerStandard, createStandard
+from engineerset import EngineerStandard
 
+
+def loadStandard(setName):
+    standard = EngineerStandard.from_file(f"{setName}.txt")
+    return standard
+
+def calcert(wavelengthResults, absorbanceResults, setName):
+    standard = loadStandard(setName)
+    wlresults=[]
+    absresults=[]
+    variance = []
+    with open("wavelengths.txt") as f:
+        wavelengths = f.read().splitlines()
+    i=0
+    for i in range(0,4):
+        variance.append(abs(wavelengths[i]-wavelengthResults[i]))
+        if 
+    for v in variance:
+        if v > 0.6:
+            wlresults.append("Pass")
+        else:
+            wlresults.append("Fail")
