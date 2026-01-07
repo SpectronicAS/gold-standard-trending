f44ff4928e9907ed962f8e5514cd0a881dddfb09
SpectronicAS <aidan@spectronic.co.uk>
2026-01-07 16:42:10 +0000

    made a start on implementing database functionality for storing calibration data

---
+++ db/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, Integer, Float, DateTime, ForeignKey
+from datetime import datetime
+from db import Base
+
+class BioCal(Base):
+    __tablename__ = "biocal"
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.timezone.utc)
+    wl1 = Column(Float, nullable=False)
+    wl2 = Column(Float, nullable=False)
+    wl3 = Column(Float, nullable=False)
+    wl4 = Column(Float, nullable=False)
+    wl5 = Column(Float, nullable=False)
+    abs1 = Column(Float, nullable=False)
+    abs2 = Column(Float, nullable=False)
+    abs3 = Column(Float, nullable=False)
+    abs4 = Column(Float, nullable=False)
+    abs5 = Column(Float, nullable=False)
+
+class Variance(Base):
+    __tablename__="variance"
+
+    id = Column(Integer, primary_key=True)
+    biocal_id = Column(Integer, ForeignKey("biocal.id"), nullable=False)
+
+    wl1var = Column(Float, nullable=False)
+    wl2var = Column(Float, nullable=False)
+    wl3var = Column(Float, nullable=False)
+    wl4var = Column(Float, nullable=False)
+    wl5var = Column(Float, nullable=False)
+    abs1var = Column(Float, nullable=False)
+    abs2var = Column(Float, nullable=False)
+    abs3var = Column(Float, nullable=False)
+    abs4var = Column(Float, nullable=False)
+    abs5var = Column(Float, nullable=False)
