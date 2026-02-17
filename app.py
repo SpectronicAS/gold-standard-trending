905756cb5f3ba9d7d410c68871a7d6efc58723c4
SpectronicAS <aidan@spectronic.co.uk>
2026-02-17 15:28:19 +0000

    added dialog box to add scans

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from db.db import engine
-from db.models import Base
+#from db.db import engine
+#from db.models import Base
+from gui.gui import MainWindow, app
 
 
-Base.metadata.create_all(bind=engine)
+
+window = MainWindow()
+window.show()
+
+app.exec()
+
+
+#Base.metadata.create_all(bind=engine)
--- gui/gui.py
+++ gui/gui.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QDialog, QDialogButtonBox, QLineEdit, QListWidget, QPushButton, QGridLayout, QHBoxLayout, QVBoxLayout, QLabel, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QDialog, QDialogButtonBox, QLineEdit, QListWidget, QPushButton, QGridLayout, QHBoxLayout, QVBoxLayout, QLabel, QWidget, QFileDialog
 
 
 
@@ -16,10 +16,11 @@ class MainWindow(QMainWindow):
         layout3 = QGridLayout()
         layout4 = QGridLayout()
 
-        addBtn = QPushButton("Add Cal")
-        delBtn = QPushButton("Delete Cal")
-        editBtn = QPushButton("Edit Cal")
-        calcBtn = QPushButton("Calculate Stats")
+        addBtn = QPushButton("Add Calibration")
+        addBtn.clicked.connect(self.new_cal)
+        delBtn = QPushButton("Delete Calibration")
+        editBtn = QPushButton("Edit Calibration")
+        calcBtn = QPushButton("Calculate Statistics")
 
         self.inputLine = QLineEdit("")
         self.inputLine.setPlaceholderText("Search Bar")
@@ -60,20 +61,70 @@ class BioCalDialog(QDialog):
     def __init__(self, parent = None):
         super().__init__(parent)
 
-        self.setWindowTitle("Add BioCal Results")
+        self.setWindowTitle("Add Bio-Cal Results")
 
         QBtn = (
             QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
         )
 
+        layout1 = QVBoxLayout()
+        layout2 = QHBoxLayout()
+        layout3 = QHBoxLayout()
+
+        self.wlLocation = QLineEdit()
+        self.wlLocation.setPlaceholderText("Enter File Location of WL Scan")
+        self.absLocation = QLineEdit()
+        self.absLocation.setPlaceholderText("Ender File Location of Abs Sample")
+        btn = QPushButton()
+        btn2 = QPushButton()
+        label = QLabel("Add Bio-Cal Results:")
+        label.setStyleSheet("font-weight:bold; font-size: 18px")
+
+        layout1.addWidget(label)
+        layout1.addStretch(20)
+        
+        layout2.addWidget(self.wlLocation)
+        btn.clicked.connect(self.browse_wl)
+        btn.setText("Browse")
+        layout2.addWidget(btn)
+
+        layout1.addLayout(layout2)
+
+        layout3.addWidget(self.absLocation)
+        btn2.clicked.connect(self.browse_abs)
+        btn2.setText("Browse")
+        layout3.addWidget(btn2)
+
+        layout1.addLayout(layout3)
 
         self.buttonBox = QDialogButtonBox(QBtn)
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
 
+        layout1.addWidget(self.buttonBox)
+
+        widget = QWidget()
+        widget.setLayout(layout1)
+        self.setLayout(layout1)
+        self.setFixedSize(500,300)
+
+    def browse_wl(self):
+        file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Select File",
+            "",
+            "Wavelength Scans (*.sca)"
+        )
+        if file_path:
+            self.wlLocation.setText(file_path)
+
+    def browse_abs(self):
+        file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Select File",
+            "",
+            "Absorbance Samples (*.qua)"
+        )
+        if file_path:
+            self.wlLocation.setText(file_path)
 app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-app.exec()
